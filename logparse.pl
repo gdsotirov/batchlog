0cf445ba63fbac51ffd2873e7a26ce3f1369df75
Georgi D. Sotirov <gdsotirov@dir.bg>
2006-08-21 20:31:15 +0000

    Initial revision

---
+++ logparse.pl
@@ -0,0 +1,94 @@
+#!/usr/bin/perl
+# Batch chain log parser for GraphTalk AIA
+# Written by Georgi D. Sotirov <gsotirov@obs.bg>
+# Date: 2006-08-21
+#
+# This script will output the main information about the batches
+# in CSV (Comma Separated Values) format.
+#
+# To use it make sure the file is executable at least to its owner:
+# $ chmod u+x logparse.pl
+# And then run it with at one argument - path to the log file
+# $ ./logparse.pl my_log_file
+#
+
+(@ARGV == 1) or die "Usage: $0 <log file>\n";
+  
+$logfile = $ARGV[0];
+
+sub reformat_date {
+  $_[0] =~ s/([0-9]{2})([0-9]{2})([0-9]{4})/$1\/$2\/$3/;
+  return $_[0];
+}
+
+open(LOGFILE, $logfile) or die "Error: Can't open file $logfile!";
+
+my $number = 0;
+my %table;
+
+# Put data in a hash since it can be spred in the file
+while ( <LOGFILE> ) {
+  my @VALS = split(/\s+/, $_);
+  (scalar(@VALS) > 1) or next;
+  my $batch = $VALS[1];
+
+  if ( ! $table{$batch}{'order'} ) {
+    $table{$batch}{'order'} = ++$number;
+  }
+
+  if ( $VALS[2] eq "debute" ) {
+    $table{$batch}{'start_date'} = reformat_date($VALS[4]);
+    $table{$batch}{'start_time'} = $VALS[6];
+  }
+  elsif ( $VALS[2] eq 'termine' ) {
+    $table{$batch}{'end_date'} = reformat_date($VALS[4]);
+    $table{$batch}{'end_time'} = $VALS[6];
+  }
+  elsif ( $VALS[2] =~ /[Rr]etour:?/ || $VALS[3] =~ /[Rr]etour:?/ ) {
+    $table{$batch}{'return'} = $VALS[4];
+  }
+  elsif ( $VALS[2] eq 'NBERR' ) {
+    my $i = 3;
+    my $err = 0;
+    while ( $VALS[$i] =~ /[0-9]+/ ) {
+      if ( $VALS[$i] ) {
+        $err += $VALS[$i];
+      }
+      ++$i;
+    }
+    $table{$batch}{'errors'} = $err;
+  }
+  elsif ( $VALS[2] =~ 'NBIT' ) {
+    my $i = 3;
+    my $step = 0;
+    while ( $VALS[$i] =~ /[0-9]+/ ) {
+      if ( $VALS[$i] ) {
+        $step += $VALS[$i];
+      }
+      ++$i;
+    }
+    $table{$batch}{'steps'} = $step;
+  }
+}
+
+# Find data order
+my @order;
+foreach $key (keys %table) {
+  $order[$table{$key}{'order'}] = $key;
+}
+
+# Print gathered information
+print "batch;start_date;start_time;end_date;end_time;return;steps;errors\n";
+for (my $i = 1; $i < scalar(@order); ++$i ) {
+  printf "%s;%s;%s;%s;%s;%d;%d;%d\n", $order[$i],
+                                      $table{$order[$i]}{'start_date'},
+                                      $table{$order[$i]}{'start_time'},
+                                      $table{$order[$i]}{'end_date'},
+                                      $table{$order[$i]}{'end_time'},
+                                      $table{$order[$i]}{'return'},
+                                      $table{$order[$i]}{'steps'},
+                                      $table{$order[$i]}{'errors'};
+}
+
+exit 0;
+
